eff7f741306562fa2d69343fcab24943fad59aec
abhinav bajpai <bajpaiabhinav64@gmail.com>
2026-01-31 01:29:20 +0530

    Refactor array merging and add new array logic
    Fixed syntax errors and added new array processing logic.

--- practice problem 1.js
+++ practice problem 1.js
@@ -4,4 +4,13 @@ for (var i = 0; i < arr2.length; i++){
     arr1.push(arr2[i]);
     
 }
+
 console.log(arr1);
+
+arrr 1 = [2,3,4,5,6];
+arrr 2 = [7,8,9,10,11];
+for (j=0;j<arr2.length;j++)
+{arrr1.push(arrr2(j)
+ console.log(arrr1);
+}
+
