d712293d74728688f87534d1dc50acde8c90d16e
abhinav bajpai <bajpaiabhinav64@gmail.com>
2026-02-03 18:33:03 +0530

    Add files via upload

---
+++ abhi.js
@@ -0,0 +1,15 @@
+// let div  = document.querySelector("div");
+// console.dir(div);
+
+// let heading = document.querySelector("h1");
+// console.dir(heading);
+
+// let divs = document.querySelectorAll(".box");
+
+// let idx  = 1;
+
+
+// for (div of divs){
+//     div.innerText = `new unique value ${idx}`;
+//     idx++;
+// }
