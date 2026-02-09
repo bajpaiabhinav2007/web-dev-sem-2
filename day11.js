fe5769ba9e957e47d071548c071514b7b72ad2da
abhinav bajpai <bajpaiabhinav64@gmail.com>
2026-02-09 16:00:13 +0530

    Add files via upload

---
+++ day11.js
@@ -0,0 +1,36 @@
+// "'"event topic ni advance javascript"""
+
+// function handleClick(){
+//     // const head = document.querySelector("h1");
+//     // head.style.color = "tomato"
+//     // head.textContent = "new heading"
+// const spanelement = document.createElement("span").textContent = "this is my span";
+
+    
+
+// const image = document.createElement("img");
+// image.setAttribute("src","https://mycareersview.com/afile/mcv23096_5vfile_krmu.jpg");// date method 
+
+// image.setAttribute("alt","K.R. Mangalam University");
+
+// document.getElementById("content").append(spanelement);
+// document.getElementById("content").append(image);
+  
+    
+// }
+
+document.getElementById("btn").addEventListener("click",()=>{
+    const spanelement = document.createElement("span").textContent = "this is my span";
+
+    
+
+const image = document.createElement("img");
+image.setAttribute("src","https://mycareersview.com/afile/mcv23096_5vfile_krmu.jpg");// date method 
+
+image.setAttribute("alt","K.R. Mangalam University");
+
+document.getElementById("content").append(spanelement);
+document.getElementById("content").append(image);
+})
+
+
